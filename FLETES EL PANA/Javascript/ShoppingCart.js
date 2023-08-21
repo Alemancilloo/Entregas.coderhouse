d7c8d3c49f5d5e039164eb865d71dba9a59c6b13
Daniel Llobet Tovar <daniel.llobet.tovar@gmail.com>
2023-08-21 15:02:43 -0400

    Agregado nuevo button

--- FLETES EL PANA/Javascript/ShoppingCart.js
+++ FLETES EL PANA/Javascript/ShoppingCart.js
@@ -25,3 +25,14 @@ function interfazCart() {
     } else {
         amountInter.innerText = 0;
     }}
+
+// AGREGADO DE BOTON EN EL DOM DEL INDEX  //
+const buttonDiv = document.querySelector(".contenedor3");
+const buttonClick = document.createElement("button");
+buttonClick.textContent = "Click Aqui";
+buttonClick.id = "buttonClick";
+buttonClick.className = "buttonClick";
+buttonDiv.appendChild(buttonClick);
+buttonClick.addEventListener("click", () => {
+    window.location.href = "./Cotizador.html";
+})
